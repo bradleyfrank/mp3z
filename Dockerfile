74a84e5d800e407d8bb4b287034991334d5d1392
Bradley Frank <bradfrank@fastmail.com>
2024-03-26 07:40:57 -0400

    WIP

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM  debian:latest
 ENV   DEBIAN_FRONTEND=noninteractive
 RUN   useradd --create-home mp3z \
         && apt-get update \
-        && apt-get --no-install-recommends -y install ffmpeg \
+        && apt-get --no-install-recommends -y install ffmpeg beets \
         && apt-get clean \
-        && pip install --no-cache-dir beets \
-        && mkdir -p /home/mp3z/Music/{Library,Untagged,Archive} /home/mp3z/.config/beets \
+        && mkdir -p \
+          /home/mp3z/.config/beets \
+          /home/mp3z/Music/Library \
+          /home/mp3z/Music/Untagged \
+          /home/mp3z/Music/Archive \
         && chown -R mp3z:mp3z /home/mp3z /usr/local/bin
 USER  mp3z
 COPY --chown=mp3z:mp3z --chmod=755 mp3z.sh /usr/local/bin/mp3z
--- mp3z.sh
+++ mp3z.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-ROOT_DIR="${HOME}/Music"
-SOURCE="${ROOT_DIR}/Untagged"
-BEETS_DB="${ROOT_DIR}/beets.db"
+SCRIPT_DIR="$(dirname -- "$( readlink -f -- "$0"; )")"
+MUSIC_DIR="${HOME}/Music"
+SOURCE_DIR="${MUSIC_DIR}/Untagged"
+ARCHIVE_DIR="${MUSIC_DIR}/Archive"
+BEETS_DB="${MUSIC_DIR}/beets.db"
+BEETS_CONFIG="${SCRIPT_DIR}/config.yaml"
 
 
 rm -f "$BEETS_DB"
-pushd "$SOURCE" &> /dev/null || exit 1
+[[ ! -d "$ARCHIVE_DIR" ]] && mkdir "$ARCHIVE_DIR"
+pushd "$SOURCE_DIR" &> /dev/null || exit 1
 
 
 while read -r basedir; do
@@ -29,12 +33,12 @@ while read -r album; do
   echo $'\uf066' " Archiving ${dirname}"
   tarfile="${dirname//[^a-zA-Z0-9]/_}.tar.gz"
   tar -czf "$tarfile" "$album"
-  mv -f "$tarfile" "${ARCHIVE}/${tarfile}"
+  mv -f "$tarfile" "${ARCHIVE_DIR}/${tarfile}"
 done < <(find . -type f -name '*.flac' -printf '%h\n' | sort -u)
 
 
 echo $'\u266f' " Importing with beets"
-beet import "$SOURCE"
+beet --config="$BEETS_CONFIG" import "$SOURCE_DIR"
 
 
 popd &> /dev/null || exit 1
